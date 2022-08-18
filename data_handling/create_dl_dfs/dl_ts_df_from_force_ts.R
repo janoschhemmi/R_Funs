2a3a8c4f917a940c0b99e4da71b8d0e030704d44
janoschhemmi <geo_jahe@geo.hu-berlin.de>
2022-08-18 10:51:37 +0200

    added complete ts

--- data_handling/create_dl_dfs/dl_ts_df_from_force_ts.R
+++ data_handling/create_dl_dfs/dl_ts_df_from_force_ts.R
@@ -77,7 +77,7 @@ bm_select_ref_and_extract_dl_df <- function(L_ts, df_inters,classes_to_include, 
                           years_prior_stable, ids_to_exclude = ids_to_exclude, index_list
                           ){
   
-  set.seed(102)
+  set.seed(85)
   ## filter_ref
   df_inters_filtered <- df_inters %>% 
     filter(change_process %in% classes_to_include) %>%
@@ -188,10 +188,118 @@ bm_select_ref_and_extract_dl_df <- function(L_ts, df_inters,classes_to_include, 
   return(selected_ts_2)
 }
 
+## output all time series 
+bm_extract_dl_df <- function(L_ts, df_inters,classes_to_include = c("Harvest","Other","Fire","Growth","Wind","Decline","Hydrology"), number_of_samples, ids_to_exclude = ids_to_exclude, index_list
+){
+  
+  set.seed(102)
+  df_inters_filtered <- df_inters %>% 
+     filter(change_process %in% classes_to_include) %>%
+     #group_by(plotid) %>%
+     #mutate(dist_prior = image_year - lag(image_year)) %>%
+     #ungroup() %>%
+     mutate(instance = row_number())
+  # 
+  
+  df_inters_filtered <- df_inters_filtered[!df_inters_filtered$plotid %in% ids_to_exclude,]
+  df_inters_filtered <- df_inters_filtered[df_inters_filtered$plotid %in% L_ts$id,]
+  
+  df_inters_filtered <- df_inters_filtered %>%
+    mutate(date_ref = strptime(paste(image_year, image_julday), format="%Y %j"))
+  
+  #head(L_ts)
+  #L_ts_s <- L_ts
+  #L_ts <- L_ts_s[1:400000,]
+  
+  ## selct closest observation for each ref 
+  closest_observations <- L_ts %>% filter(.$id %in% df_inters_filtered$plotid) %>% 
+    filter(index == "NBR") %>%
+    left_join(df_inters_filtered, by = c("id"="plotid")) %>%
+    mutate(diff = abs(as.numeric(difftime(date, date_ref, units = "days")))) %>%
+    group_by(id, instance) %>%
+    slice(which.min((diff))) %>%
+    ungroup() %>%
+    select(id,date,change_process,diff, instance) %>%
+    as.data.frame()
+  
+  #table(closest_observations$change_process)
+  
+  ## get padding days for each index 
+  ## left_join_by date 
+  selected_ts <- L_ts %>% filter(index %in% index_list) %>%
+    filter(id %in% df_inters_filtered$plotid) %>% 
+    left_join(.,closest_observations, by = c("id" = "id", "date" = "date")) #%>%
+  
+  # GetIDsBeforeAfter <- function(x) {
+  #   v = (x-padding_days) : (x+padding_days)
+  # }
+  
+  ## row ids with padding
+  # selected_row_ids <- selected_ts %>%
+  #   mutate(row_id = row_number()) %>%
+  #   filter((!is.na(change_process))) %>%
+  #   pull(row_id) %>%
+  #   map(GetIDsBeforeAfter) %>%
+  #   unlist()
+  # 
+  # selected_ts_2 <- selected_ts[selected_row_ids,]
+  
+  
+  ## expant instances and change process rows
+  # GetrepsBeforeAfter <- function(x) {
+  #   times = 2 * padding_days + 1  ## how many times replicate pulled instance 
+  #   replicated <- rep(x, times)
+  #   return(replicated)
+  # }
+  # 
+  # selected_reps <- selected_ts %>%
+  #   mutate(row_id = row_number()) %>%
+  #   filter((!is.na(.$instance))) %>%
+  #   pull(instance) %>%
+  #   map(GetrepsBeforeAfter) %>%
+  #   unlist()
+  # 
+  # selected_ts_2$instances_rep <- selected_reps
+  # 
+  # ## add sequence
+  # Getsequence <- function(x) {
+  #   times = 2 * padding_days + 1  ## how many times replicate pulled instance 
+  #   seq <- seq(1, times)
+  #   return(seq)
+  # }
+  # selected_sequence <- selected_ts %>%
+  #   mutate(row_id = row_number()) %>%
+  #   filter((!is.na(.$instance))) %>%
+  #   pull(instance) %>%
+  #   map(Getsequence) %>%
+  #   unlist()
+  # 
+  # selected_ts_2$time_seq <- selected_sequence
+  # 
+  # selected_changes <- selected_ts %>%
+  #   mutate(row_id = row_number()) %>%
+  #   filter((!is.na(.$instance))) %>%
+  #   pull(change_process) %>%
+  #   map(GetrepsBeforeAfter) %>%
+  #   unlist()
+  # selected_ts_2$changes_rep <- selected_changes
+  
+  # add sequence number 
+  selected_ts <- selected_ts %>%
+    group_by(id, index) %>%
+    mutate(time_sequence = row_number()) %>%
+    ungroup() %>%
+    as.data.frame()
+  
+  
+  return(selected_ts)
+}
+
 
 
 
 ## Apply 
+## #1 select sliced time window 
 
 df_for_dl <- bm_select_ref_and_extract_dl_df(L_ts=L_ts,
                                              df_inters = df_inters,
@@ -220,7 +328,24 @@ y_safe[y_safe == "Other"] <- 3
 y_safe[y_safe == "Stable"] <- 4
 y_safe <- as.integer(y_safe)
 
-write.csv2(y_safe, paste0("P:/workspace/jan/fire_detection/dl/prepocessed_ref_tables/02_df_y_",padding_days,".csv"),row.names = FALSE)
-write.table(x_safe, paste0("P:/workspace/jan/fire_detection/dl/prepocessed_ref_tables/02_df_x_",padding_days,".csv"), row.names = FALSE, col.names = TRUE, dec = ".", sep = ";")
+write.csv2(y_safe, paste0("P:/workspace/jan/fire_detection/dl/prepocessed_ref_tables/02_df_y_",padding_days,"_2.csv"),row.names = FALSE)
+write.table(x_safe, paste0("P:/workspace/jan/fire_detection/dl/prepocessed_ref_tables/02_df_x_",padding_days,"_2.csv"), row.names = FALSE, col.names = TRUE, dec = ".", sep = ";")
+
+##
+## #2 all time series 
+
+df_for_dl <- bm_extract_dl_df(L_ts[1:50000,], df_inters,classes_to_include = c("Harvest","Other","Fire","Growth","Wind","Decline","Hydrology"), 
+                              ids_to_exclude = ids_to_exclude, index_list = c("NBR", "NDV", "TCW", "TCG", "TCB")
+)
+table(df_for_dl$change_process)
+
+df_for_dl_wide <- df_for_dl %>% select(!c(change_process,date,diff,instance, sensor)) %>% pivot_wider(names_from = c(time_sequence),values_from = c(value)) %>%
+  arrange(.,id,index) %>% as.data.frame()
+
+# df_for_dl_wide_change_process <- df_for_dl %>% select(!c(date,diff,instance, sensor)) %>% pivot_wider(names_from = c(time_sequence),values_from = c(change_process)) %>%
+#   arrange(.,id,index) %>% replace_na(is.na(.),0) %>% as.data.frame()
+
+write.table(df_for_dl_wide, paste0("P:/workspace/jan/fire_detection/dl/prepocessed_ref_tables/03_df_x_50000_ts.csv"), row.names = FALSE, col.names = TRUE, dec = ".", sep = ";")
+
 
 
